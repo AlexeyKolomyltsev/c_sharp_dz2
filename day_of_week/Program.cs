0511f4a479f9b9e02e1db931222dffa670739e89
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-14 12:21:15 +0800

    add дни недели

---
+++ day_of_week/Program.cs
@@ -0,0 +1,23 @@
+﻿var week = new Dictionary<string, string>() /// Добавил словарь с днями недели
+{
+    {"1", "понедельник"},
+    {"2", "вторник"},
+    {"3", "среда"},
+    {"4", "четверг"},
+    {"5", "пятница"},
+    {"6", "суббота"},
+    {"7", "воскресенье"},
+};
+
+Console.WriteLine("Ввести день недели");
+string day = Console.ReadLine();
+if (week.TryGetValue(day, out string value)) ///Проверка наличия номера дня недели
+{
+    if (day == "6" || day == "7")
+    {
+        Console.WriteLine($"{day} день недели выходной - {value}");;
+    }
+    else Console.WriteLine($"{day} день недели будний - {value}");
+}
+else Console.WriteLine("Нет такого дня недели");
+
