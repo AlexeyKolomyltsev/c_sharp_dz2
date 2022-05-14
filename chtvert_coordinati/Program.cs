36d54222f1816ceef3c49d69170285e337222748
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-14 16:04:08 +0800

    исправил код для задачи по определентю четвертей

--- chtvert_coordinati/Program.cs
+++ chtvert_coordinati/Program.cs
@@ -2,8 +2,8 @@
 
 Console.WriteLine("Напишите номер четверти");
 string number = Console.ReadLine();
-if (number == "1") Console.WriteLine("Коордитаты x>0, y>0");
-else if (number == "2") Console.WriteLine("Коордитаты x<0, y>0");
-else if (number == "3") Console.WriteLine("Коордитаты x<0, y<0");
-else if (number == "4") Console.WriteLine("Коордитаты x>0, y<0");
-else Console.WriteLine("Нет такой плоскости");
+if (number == "1") Console.WriteLine($"Для {number} четверти коордитаты x>0, y>0");
+else if (number == "2") Console.WriteLine($"Для {number} четверти коордитаты x<0, y>0");
+else if (number == "3") Console.WriteLine($"Для {number} четверти коордитаты x<0, y<0");
+else if (number == "4") Console.WriteLine($"Для {number} четверти коордитаты x>0, y<0");
+else Console.WriteLine("Нет такой четверти");
