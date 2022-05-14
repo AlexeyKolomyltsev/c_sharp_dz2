b58e4788a9352960a4932162685f7d99f9cf647b
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-14 15:40:38 +0800

    add задачу расстояния между точками в n пространстве

---
+++ rasstojanie_mejdu_tochkami/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите размерность пространства (больше 1)");
+int stepenS = int.Parse(Console.ReadLine());
+double[] coordinates1 = new double[stepenS]; ///Массив для координат первой точки
+double[] coordinates2 = new double[stepenS]; ///Массив для координат первой точки
+double DistKvadrat = 0;
+for (int i = 0; i < stepenS; i++)  ///Внесение координат для точек
+{
+    Console.Write($"Введите {i + 1} коррдинату для первой точки ");
+    coordinates1[i] = double.Parse(Console.ReadLine());
+    Console.Write($"Введите {i + 1} коррдинату для второй точки ");
+    coordinates2[i] = double.Parse(Console.ReadLine());
+    DistKvadrat += (coordinates1[i] - coordinates2[i]) * (coordinates1[i] - coordinates2[i]);
+}
+Console.WriteLine("Координаты первой точки ({0})", String.Join("; ", coordinates1));
+Console.WriteLine("Координаты второй точки ({0})", String.Join("; ", coordinates2));
+Console.WriteLine("Расстояние между точками = " + Math.Sqrt(DistKvadrat));
