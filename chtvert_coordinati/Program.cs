7e3ba4ce5f17ebe21f3a4cb42d7e5da44cbdd600
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-14 13:27:03 +0800

    add координаты

---
+++ chtvert_coordinati/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Напишите номер четверти");
+string number =Console.ReadLine();
+if (number == "1") Console.WriteLine("Коордитаты x>0, y>0");
+else if (number == "2") Console.WriteLine("Коордитаты x<0, y>0");
+else if (number == "3") Console.WriteLine("Коордитаты x<0, y<0");
+else if (number == "4") Console.WriteLine("Коордитаты x>0, y<0");
+else Console.WriteLine("Нет такой плоскости");
